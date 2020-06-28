60db5134f8736300b323257f56cea8f5bc03c8de
mateus rodrigues <mateus037@gmail.com>
2020-06-28 03:16:25 -0300

    Deletando arquivos inutilizados, aplicando tema na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createMuiTheme } from '@material-ui/core/styles';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import './App.css';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e91e63',
+    },
+    secondary: {
+      main: '#00b0ff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div>
+        <h1>Teste</h1>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
